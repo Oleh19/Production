51c614855c2d4e40627a412c72deadce6839441a
Ilchenko Oleh <deckart212@gmail.com>
2023-11-11 03:13:39 +0200

    add SVG loader, File loader, Button UI

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,13 +4,11 @@ import { AppRouter } from 'app/providers/router';
 import { Navbar } from 'widgets/Navbar';
 
 const App = () => {
-  const { theme, toggleTheme } = useTheme();
-  const bool = true;
+  const { theme } = useTheme();
 
   return (
     <div className={classNames('app', {}, [theme])}>
      <Navbar />
-      <button onClick={toggleTheme}>Toggle</button>
       <AppRouter />
     </div>
   );
